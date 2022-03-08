cebca44319273e3b46c6a3874a1ce59b27f1c1ea
octaprog7 <goctaprog@gmail.com>
2022-03-08 23:03:42 +0500

    first commit

---
+++ my_strings.py
@@ -0,0 +1,10 @@
+"""any strings for util"""
+
+strFolderHandling = "Folder handling"
+strFEF = "File extension filter"
+strCCA = "Checksum calculation algorithm"
+
+head_header = "#"
+str_start_files_header = f"{head_header}FILES{head_header}"
+str_end_files_header = str_start_files_header[::-1]
+strCS_filename_splitter = "\t"
