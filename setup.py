566c073f6a8189c7a172a7efe6a4f0938a09b749
octaprog7 <goctaprog@gmail.com>
2023-01-05 19:44:34 +0500

    correct errors

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md") as file:
     read_me_description = file.read()
 
 setuptools.setup(
-    name="cfcs-package-octaprog7",
+    name="calc_files_control_sum",
     version="1.0",
-    author="Roman Kaban",
+    author="Roman Shevchik",
     author_email="goctaprog@gmail.com",
     description="calculate files control sum",
     long_description=read_me_description,
     long_description_content_type="text/markdown",
     url="https://github.com/octaprog7/CalcFilesControlSum",
-    packages=setuptools.find_packages("src"),
+    py_modules=["cfcs", "my_utils", "my_strings", "config"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -24,6 +25,10 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
     ],
-    package_dir={"": "src"},
     python_requires='>=3.7.2',
+    entry_points={
+        'console_scripts': [
+            'cfcs = cfcs:main',
+        ],
+    }
 )
